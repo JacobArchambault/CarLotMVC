9b14705f290bcaa302ef169401915c57079dfdda
Jacob Archambault <jacob.archambault@waystar.com>
2020-04-08 13:17:44 -0400

    Add AutoLotDAL and AutoLotDAL.Models to project. Add reference from CarLotMVC to both; add Entity Framework to MVC project; add associated connection string to MVC project's Web.config; add Database.SetInitializer to Global.asax.cs.

--- CarLotMVC/Global.asax.cs
+++ CarLotMVC/Global.asax.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AutoLotDAL.EF;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,7 +7,7 @@ using System.Web.Routing;
 
 namespace CarLotMVC
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +15,7 @@ namespace CarLotMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new MyDataInitializer());
         }
     }
 }
